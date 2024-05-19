67d4b609bd01aea3cf5ec8d1db8f793e19ffa868
david setyawan <45872505+davidsetyawan@users.noreply.github.com>
2024-05-19 11:22:30 +0700

    delete df_links apply int64

--- data.py
+++ data.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 
 def load_data(ratings_file, movies_file, links_file):
-    df_m = pd.read_csv(movies_file)
-    df = pd.read_csv(ratings_file)
-    df_l = pd.read_csv(links_file)
-    df_l['tmdbId'] = df_l['tmdbId'].apply("int64")
-    return df, df_m, df_l
+    df_ratings = pd.read_csv(ratings_file)
+    df_movies = pd.read_csv(movies_file)
+    df_links = pd.read_csv(links_file)
+    return df_ratings, df_movies, df_links
